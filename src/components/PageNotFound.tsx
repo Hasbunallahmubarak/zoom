f1912a180b1c8d4296e124605a5f766040b1537b
Hasbunallah Mubarak Abiola <hasbunallah2003@gmail.com>
2025-08-01 12:28:04 +0100

    Restructed the 'Page Not Found' webpage

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div className="h-screen bg-base-200 border-3 flex justify-center content-center">
-      <div className=" p-10 ">
-        <h1 className="text-3xl font-bold  opacity-70">Page Not found</h1>
-        <Link to={"/"}>Back to Homepage</Link>
+    <div className=" h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-10 mt-[-20vh]  opacity-70">
+          Page Not found
+        </h1>
+        <Link to={"/"} className="btn btn-error">
+          Back to Homepage
+        </Link>
       </div>
     </div>
   );
